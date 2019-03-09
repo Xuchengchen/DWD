ca1a8398b45374c8d4b86a97610cba712b7bc8bf
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2019-03-09 17:35:31 +0000

    type-and-tag

--- data/data-extraction.py
+++ data/data-extraction.py
@@ -1,22 +1,42 @@
 import json
 
 genres = set()
-tags = set()
+# tags = set()
 
 with open('fringe-fake-data.json') as file:
     fringe = json.load(file)
+    print(len(fringe))
     for show in fringe:
-        genres.add(show["genre"])
-        if show["genre_tags"] != None:
-            for i in show["genre_tags"].split(","):
-                tags.add(i)
+        genres.add((show["genre"],show["genre_tags"]))
+        # if show["genre_tags"] != None:
+        #     for i in show["genre_tags"].split(","):
+        #         tags.add(i)
 
-print("genres")
-print("=======================")
+
+# print(genres[0]+","+genres[1])
+# print("=======================")
+
+
 for item in genres:
-    print(item)
-print("=======================")
-print("tags")
-print("=======================")
-for item in tags:
-    print(item)
+    if item[1] == None:
+        continue
+    else:
+        # print(item[1].split(","))
+        for x in item[1].split(","):
+            print(item[0]+","+x)
+        # if len(item[1]) > 1:
+        #     print(item[1])
+        #     for x in item:
+        #         print(x)
+        #         print(item[0]+","+x)
+        # else:
+        #     print(item[1])
+        #     print(item[0]+","+item[1])
+
+
+#     print(item)
+# print("=======================")
+# print("tags")
+# print("=======================")
+# for item in tags:
+#     print(item)
