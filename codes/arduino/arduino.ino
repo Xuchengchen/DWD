58ead885793d6ff5bbe563332d69f1d4eee2de90
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2019-03-10 17:33:35 +0000

    codes

---
+++ codes/arduino/arduino.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  
+  // determine if the session end
+    // yes: end task, send result to computers
+  
+  // update all dials' information
+
+  // set LEDs HSL by RGB
+
+  // set music on computer
+  
+}
