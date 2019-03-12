6ff4f6328e585aa1315bd217b8fb34d8f86d56b7
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2019-03-12 16:57:19 +0000

    change lights with potentiometer

--- codes/arduino/arduino.ino
+++ codes/arduino/arduino.ino
@@ -1,10 +1,31 @@
+#include <FastLED.h>
+
+#define LED_PIN     13
+#define NUM_LEDS    16
+
+const int analogInPin = A0;
+float sensorValue = 0;
+int outputValue = 0;  
+
+CRGB leds[NUM_LEDS];
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
 
 }
 
 void loop() {
-  
+
+  sensorValue = analogRead(analogInPin);
+  outputValue = float(sensorValue / 1023) * 255;
+  for (int i = 0; i <30; i++) {
+    leds[i] = CRGB(outputValue, 18, 25);
+      FastLED.show();
+  }
+
+  //delay(1000);  
   // determine if the session end
     // yes: end task, send result to computers
   
